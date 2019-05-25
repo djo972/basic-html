71e633e2d90f1cda454714aed89ab070f66591fc
djo_972 <giovanny.pied@gmail.com>
2019-05-25 04:02:18 +0200

    modify gruntfiles (sass)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,49 @@
-module.exports = function(grunt){
-    //congigation de Grunt
-    grunt.initConfig({});
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            // Nom de la tâche
+            dev: {
+                // Nom de la sous-tâche
+                options: {
+                    // Options
+                    style: "expanded",
+                },
+                files: [
+                    {
+                        // C'est ici que l'on définit le dossier que l'on souhaite importer
+                        expand: true,
+                        cwd: "src/styles/",
+                        src: ["main.scss"],
+                        dest: "dist/styles/",
+                        ext: ".css",
+                    },
+                ],
+            },
+            prod: {
+                // Nom de la sous-tâche
+                options: {
+                    // Options
+                    style: "compressed",
+                },
+                files: [
+                    {
+                        // C'est ici que l'on définit le dossier que l'on souhaite importer
+                        expand: true,
+                        cwd: "src/styles/",
+                        src: ["main.scss"],
+                        dest: "dist/styles/",
+                        ext: ".css",
+                    },
+                ],
+            },
+        },
+    });
 
-    grunt.registerTask("default","")
+    // Import du package
+    grunt.loadNpmTasks("grunt-contrib-sass");
 
-}
+    // Redéfinition de la tâche `default` qui est la tâche lancée dès que vous lancez Grunt sans rien spécifier.
+    // Note : ici, nous définissons sass comme une tâche à lancer si on lance la tâche `default`.
+    grunt.registerTask("default",["sass:prod"]);
+    grunt.registerTask("dev",["sass:dev"]);
+};
