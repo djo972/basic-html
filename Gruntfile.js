92f65b58a382c7ce6d8bcd428502e0f8b0d19b47
djo_972 <giovanny.pied@gmail.com>
2019-05-27 00:39:07 +0200

    modify gruntfiles (watch--live_reload)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,11 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            build: {
+                src: ['dist/*']
+            }
+        },
         sass: {
             // Nom de la tâche
             dev: {
@@ -46,24 +52,63 @@ module.exports = function(grunt) {
                 dest: "dist/js/built.js", // la destination finale
             },
         },
+        babel: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    "dist/js/app.js": "dist/js/built.js"
+                }
+            }
+        },
         uglify: {
             options: {
                 separator: ";",
             },
             dist: {
-                src: ["src/js/main.js","src/js/extra.js"], // la source
-                dest: "dist/js/built.js", // la destination finale
+                src: ["dist/js/app.js"], // la source
+                dest: "dist/js/app.min.js", // la destination finale
             },
         },
+        connect: {
+            all: {
+                options:{
+                    port: 9000,
+                    hostname: 'localhost',
+                    livereload: 35729,
+                    // base: 'http://localhost/src/'
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: 35729
+            },
+            scripts: {
+                files: '**/*.js', // tous les fichiers JavaScript de n'importe quel dossier
+                tasks: ['clean']
+            },
+            styles: {
+                files: '**/*.scss', // tous les fichiers Sass de n'importe quel dossier
+                tasks: ['clean']
+            }
+        }
     });
 
     // Import du package
     grunt.loadNpmTasks("grunt-contrib-sass");
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     // Redéfinition de la tâche `default` qui est la tâche lancée dès que vous lancez Grunt sans rien spécifier.
     // Note : ici, nous définissons sass comme une tâche à lancer si on lance la tâche `default`.
-    grunt.registerTask("default",["sass:dist","concat:dist","uglify:dist"]);
+    grunt.registerTask("default",["clean","sass:dist","concat:dist","babel","uglify"]);
     grunt.registerTask("dev",["sass:dev","concat:dist"]);
+    // Start web server
+    grunt.registerTask('serve', ['connect:all','watch']);
 };
--- src/js/extra.js
+++ src/js/extra.js
@@ -1,24 +1,26 @@
-$(window).scroll(function() {
-    const
-        a = $(this).scrollTop(),
-        b = 400,
-        t = window.pageYOffset;
+// $(window).scroll(function() {
+//     const
+//         a = $(this).scrollTop(),
+//         b = 500,
+//         t = window.pageYOffset;
+//
+//     $(".parallax").css('transform', 'translate(-' + a / 1.6 + 'px, -' + 1 - a / b + 'px)');
+//
+//     $(".one").css('bottom', -(t * 0.783) + 'px');
+//     $(".two").css('bottom', -(t * 0.4) + 'px');
+//     $(".three").css('bottom', -(t * 0.5) + 'px');
+//     $(".four").css('bottom', -(t * 0.6) + 'px');
+//     $(".five").css('bottom',-(t * 0.726) + 'px');
+// });
+//
+// $("header").on('mousemove', (e) => {
+//     const cx = $(window).width() / 2,
+//         cy = $(window).height() / 2,
+//         x = ((cx - e.pageX) / cx) * 3,
+//         y = ((cy - e.pageY) / cy) * 2;
+//     $(".two").css('transform',`translate(${x}px, ${y}px)`);
+//     $(".four").css('transform',`translate(-${x}px, ${y}px)`);
+//     $(".five").css('transform',`translate(${x}px, -${y}px)`);
+// });
 
-    $(".parallax").css('transform', 'translate(-' + a / 1.6 + 'px, -' + 1 - a / b + 'px)');
-
-    $(".one").css('bottom', -(t * 0.3) + 'px');
-    $(".two").css('bottom', -(t * 0.4) + 'px');
-    $(".three").css('bottom', -(t * 0.5) + 'px');
-    $(".four").css('bottom', -(t * 0.6) + 'px');
-    $(".five").css('bottom',-(t * 0.7) + 'px');
-});
-
-$("header").on('mousemove', (e) => {
-    const cx = $(window).width() / 2,
-        cy = $(window).height() / 2,
-        x = ((cx - e.pageX) / cx) * 2,
-        y = ((cy - e.pageY) / cy) * 2;
-    $(".two").css('transform',`translate(${x}px, ${y}px)`);
-    $(".four").css('transform',`translate(-${x}px, ${y}px)`);
-    $(".five").css('transform',`translate(${x}px, -${y}px)`);
-});
+alert('afee');
